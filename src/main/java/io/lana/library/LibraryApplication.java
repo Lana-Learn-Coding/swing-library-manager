76a18a919d264df10c4d0e59bbca2033cac609a2
Lana <lanadrahrepus2552@gmail.com>
2020-12-20 18:18:06 +0700

    setup: add flatlaf

--- src/main/java/io/lana/library/LibraryApplication.java
+++ src/main/java/io/lana/library/LibraryApplication.java
@@ -1,12 +1,14 @@
 package io.lana.library;
 
-import io.lana.library.ui.LoginUI;
+import com.formdev.flatlaf.FlatLightLaf;
+import io.lana.library.ui.view.LoginUI;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryApplication {
 
     public static void main(String[] args) {
+        FlatLightLaf.install();
         new LoginUI().setVisible(true);
     }
 
