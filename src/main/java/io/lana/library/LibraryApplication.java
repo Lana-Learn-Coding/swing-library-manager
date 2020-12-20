da99c11a1c51712e9c81f98bb689df1e1960f714
Lana <lanadrahrepus2552@gmail.com>
2020-12-21 00:27:31 +0700

    fix: fix application startup

--- src/main/java/io/lana/library/LibraryApplication.java
+++ src/main/java/io/lana/library/LibraryApplication.java
@@ -2,14 +2,23 @@ package io.lana.library;
 
 import com.formdev.flatlaf.FlatLightLaf;
 import io.lana.library.ui.view.LoginUI;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class LibraryApplication {
+public class LibraryApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
+        new SpringApplicationBuilder(LibraryApplication.class)
+            .headless(false)
+            .run(args);
+    }
+
+
+    @Override
+    public void run(String... args) {
         FlatLightLaf.install();
         new LoginUI().setVisible(true);
     }
-
 }
