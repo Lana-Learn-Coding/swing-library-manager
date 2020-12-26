a92dec6701ccd92046dd6760ae47013693ed3555
Lana <lanadrahrepus2552@gmail.com>
2020-12-26 23:29:36 +0700

    dev: maintain original data inside panel

--- src/main/java/io/lana/library/ui/component/app/AbstractTablePane.java
+++ src/main/java/io/lana/library/ui/component/app/AbstractTablePane.java
@@ -2,18 +2,16 @@ package io.lana.library.ui.component.app;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.util.List;
-import java.util.Vector;
+import java.util.*;
 
 public abstract class AbstractTablePane<T> extends JScrollPane {
+    protected final List<T> data = new ArrayList<>();
     protected final JTable table = new JTable();
+    private final DefaultTableModel tableModel;
 
     public AbstractTablePane() {
-        initComponents();
-    }
-
-    private void initComponents() {
-        table.setModel(new DefaultTableModel(getTableColumns(), 0));
+        tableModel = new DefaultTableModel(getTableColumns(), 0);
+        table.setModel(tableModel);
         setViewportView(table);
     }
 
@@ -26,9 +24,60 @@ public abstract class AbstractTablePane<T> extends JScrollPane {
     }
 
     public void setTableData(List<T> data) {
-        DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
+        this.data.clear();
+        this.data.addAll(data);
         tableModel.setRowCount(0);
         data.forEach(model -> tableModel.addRow(toTableRow(model)));
         tableModel.fireTableDataChanged();
+    }
+
+    public void removeRow(int index) {
+        data.remove(index);
+        tableModel.removeRow(index);
+        tableModel.fireTableDataChanged();
+    }
+
+    public void removeSelectedRow() {
+        removeRow(table.getSelectedRow());
+    }
+
+    public T getRow(int index) {
+        return data.get(index);
+    }
+
+    public T getSelectedRow() {
+        return getRow(table.getSelectedRow());
+    }
+
+    public int getSelectedRowIndex() {
+        return table.getSelectedRow();
+    }
+
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    public void addRow(T rowData) {
+        data.add(rowData);
+        tableModel.addRow(toTableRow(rowData));
+        tableModel.fireTableDataChanged();
+    }
+
+    public void addRow(int index, T rowData) {
+        data.add(index, rowData);
+        tableModel.insertRow(index, toTableRow(rowData));
+        tableModel.fireTableDataChanged();
+    }
+
+    public void clearSelection() {
+        table.clearSelection();
+    }
+
+    public ListSelectionModel getSelectionModel() {
+        return table.getSelectionModel();
+    }
+
+    public void setSelectedRow(int index) {
+        table.setRowSelectionInterval(index, index);
     }
 }
