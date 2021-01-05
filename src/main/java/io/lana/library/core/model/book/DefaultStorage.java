a87208539b4289dd6d46213e28e5774e47734831
Lana <lanadrahrepus2552@gmail.com>
2021-01-05 16:10:04 +0700

    refactor: remove unused

--- src/main/java/io/lana/library/core/model/book/DefaultStorage.java
+++
@@ -1,6 +0,0 @@
-package io.lana.library.core.model.book;
-
-public enum DefaultStorage {
-    SHELF,
-    STORAGE
-}
--- src/main/java/io/lana/library/core/model/user/Role.java
+++
@@ -1,4 +0,0 @@
-package io.lana.library.core.model.user;
-
-public class Role {
-}
