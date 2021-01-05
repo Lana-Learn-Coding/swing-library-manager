efe366708b5bc60eecb745f4fea9def8172d4300
Lana <lanadrahrepus2552@gmail.com>
2021-01-05 16:09:46 +0700

    dev: add loading screen

--- src/main/java/io/lana/library/ui/view/app/InitPanel.java
+++ src/main/java/io/lana/library/ui/view/app/InitPanel.java
@@ -16,6 +16,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
 @Component
 public class InitPanel extends JPanel {
@@ -34,35 +36,114 @@ public class InitPanel extends JPanel {
         this.userContext = userContext;
         this.mainFrame = mainFrame;
         this.applicationContext = applicationContext;
+        header.setText("Welcome, " + userContext.getUser().getName());
+        subheader.setText(userContext.getUser().getUsername());
         WorkerUtils.runAsync(this::initApp);
     }
 
     private void initApp() {
+        progress.setValue(10);
+        loadingText.setText("Initializing...");
         CrudPanel<BookMeta> bookMetaManagePanel = applicationContext.getBean(BookMetaManagerPanel.class);
         CrudPanel<Reader> readerMetaManagePanel = applicationContext.getBean(ReaderManagerPanel.class);
+        progress.setValue(30);
+
+        loadingText.setText("Loading Book...");
         readerMetaManagePanel.renderTable();
+        progress.setValue(60);
+
+        loadingText.setText("Loading Reader...");
         bookMetaManagePanel.renderTable();
+        progress.setValue(90);
+
+        loadingText.setText("Getting Ready...");
+        applicationContext.getBean(MainPanel.class);
+        progress.setValue(100);
+
+        delay(1000);
         mainFrame.setContentPane(MainPanel.class);
+    }
+
+    private void delay(int millis) {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            // ignore
+        }
     }
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        mainPanel = new JPanel();
+        header = new JLabel();
+        subheader = new JLabel();
+        loadingText = new JLabel();
+        progress = new JProgressBar();
 
         //======== this ========
+        setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        //======== mainPanel ========
+        {
+            mainPanel.setLayout(new GridBagLayout());
+            ((GridBagLayout) mainPanel.getLayout()).columnWidths = new int[]{0, 0, 0, 0};
+            ((GridBagLayout) mainPanel.getLayout()).rowHeights = new int[]{0, 0, 35, 0, 0};
+            ((GridBagLayout) mainPanel.getLayout()).columnWeights = new double[]{0.0, 1.0, 0.0, 1.0E-4};
+            ((GridBagLayout) mainPanel.getLayout()).rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, 1.0E-4};
+
+            //---- header ----
+            header.setHorizontalAlignment(SwingConstants.CENTER);
+            header.setFont(new Font("Tahoma", Font.PLAIN, 15));
+            mainPanel.add(header, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(0, 0, 5, 5), 0, 0));
+
+            //---- subheader ----
+            subheader.setHorizontalAlignment(SwingConstants.CENTER);
+            subheader.setFont(new Font("Tahoma", Font.PLAIN, 11));
+            subheader.setVerticalAlignment(SwingConstants.TOP);
+            subheader.setForeground(Color.gray);
+            mainPanel.add(subheader, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(0, 0, 5, 5), 0, 0));
+
+            //---- loadingText ----
+            loadingText.setFont(loadingText.getFont().deriveFont(12f));
+            loadingText.setHorizontalAlignment(SwingConstants.CENTER);
+            loadingText.setVerticalAlignment(SwingConstants.BOTTOM);
+            loadingText.setForeground(Color.darkGray);
+            mainPanel.add(loadingText, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(0, 0, 5, 5), 0, 0));
+            mainPanel.add(progress, new GridBagConstraints(0, 3, 3, 1, 0.0, 0.0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(0, 0, 0, 0), 0, 0));
+        }
 
         GroupLayout layout = new GroupLayout(this);
         setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup()
-                .addGap(0, 385, Short.MAX_VALUE)
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(mainPanel, GroupLayout.DEFAULT_SIZE, 320, Short.MAX_VALUE)
+                    .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup()
-                .addGap(0, 190, Short.MAX_VALUE)
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(mainPanel, GroupLayout.DEFAULT_SIZE, 113, Short.MAX_VALUE)
+                    .addContainerGap())
         );
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    private JPanel mainPanel;
+    private JLabel header;
+    private JLabel subheader;
+    private JLabel loadingText;
+    private JProgressBar progress;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
