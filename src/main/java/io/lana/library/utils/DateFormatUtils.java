33f31238769331ae3a803331836bff0cc93b79f5
Lana <lanadrahrepus2552@gmail.com>
2021-01-05 21:53:30 +0700

    refactor: add date formats

--- src/main/java/io/lana/library/utils/DateFormatUtils.java
+++ src/main/java/io/lana/library/utils/DateFormatUtils.java
@@ -9,19 +9,23 @@ import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class DateFormatUtils {
-    public static final DateTimeFormatter COMMON_DATE_FORMAT = DateTimeFormatter.ofPattern("yyy-MM-dd[ HH:mm:ss]");
+    public static final DateFormat COMMON_DATE_FORMAT = new SimpleDateFormat("yyy-MM-dd");
 
-    private static final DateTimeFormatter COMMON_DATE_FORMAT_WITHOUT_TIME = DateTimeFormatter.ofPattern("yyy-MM-dd");
+    public static final DateFormat COMMON_DATETIME_FORMAT = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
+
+    public static final DateTimeFormatter COMMON_DATETIME_FORMATTER = DateTimeFormatter.ofPattern("yyy-MM-dd[ HH:mm:ss]");
+
+    public static final DateTimeFormatter COMMON_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyy-MM-dd");
 
     private DateFormatUtils() {
     }
 
     public static String toDateString(LocalDate localDate) {
-        return localDate.format(COMMON_DATE_FORMAT_WITHOUT_TIME);
+        return localDate.format(COMMON_DATE_FORMATTER);
     }
 
     public static String toDateString(LocalDateTime localDateTime) {
-        return localDateTime.format(COMMON_DATE_FORMAT);
+        return localDateTime.format(COMMON_DATETIME_FORMATTER);
     }
 
     public static String toDateString(Date date) {
@@ -52,5 +56,12 @@ public class DateFormatUtils {
     public static String toDateStringWithDefaultUnknown(Object date) {
         String parsed = toDateString(date);
         return parsed == null ? "Unknown" : parsed;
+    }
+
+    public static LocalDate toLocalDate(Date date) {
+        if (date instanceof java.sql.Date) {
+            return new java.sql.Date(date.getTime()).toLocalDate();
+        }
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
     }
 }
