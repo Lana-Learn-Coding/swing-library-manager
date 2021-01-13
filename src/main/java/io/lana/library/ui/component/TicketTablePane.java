6106869f5ea823713081d91da4449997703fa004
Lana <lanadrahrepus2552@gmail.com>
2021-01-14 01:06:24 +0700

    refactor: rename borrowing table component, update table columns

--- src/main/java/io/lana/library/ui/component/TicketTablePane.java
+++ src/main/java/io/lana/library/ui/component/TicketTablePane.java
@@ -6,20 +6,19 @@ import io.lana.library.ui.component.app.table.TableColumnMapping;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
-public class BookBorrowingTablePane extends AbstractRepoBasedTablePane<Ticket> {
+public class TicketTablePane extends AbstractRepoBasedTablePane<Ticket> {
     @Override
     protected TableColumnMapping<Ticket> getTableColumnMapping() {
         TableColumnMapping<Ticket> mapping = new TableColumnMapping<>();
         mapping.setDefaultColumnType(String.class);
         mapping.put("ID", Ticket::getId, Integer.class);
         mapping.put("Borrower", ticket -> ticket.getBorrower().getId() + " - " + ticket.getBorrower().getName(), Integer.class);
-        mapping.put("Book Count", ticket -> ticket.getBooks().size(), Integer.class);
-        mapping.put("Overdue", ticket -> LocalDate.now().isBefore(ticket.getDueDate()) ? "" : "Overdue");
-        mapping.put("Overdue Days", ticket -> LocalDate.now().isBefore(ticket.getDueDate()) ? 0 : ChronoUnit.DAYS.between(ticket.getDueDate(), LocalDate.now()), Integer.class);
+        mapping.put("Book Count", Ticket::getBooksCount, Integer.class);
+        mapping.put("Overdue Days", Ticket::getOverDueDays, Integer.class);
         mapping.put("Borrowed Since", Ticket::getBorrowedDate, LocalDate.class);
         mapping.put("Due Date", Ticket::getDueDate, LocalDate.class);
+        mapping.put("Returned Date", Ticket::getReturnedDate, LocalDate.class);
         mapping.put("Created At", Ticket::getCreatedAt, LocalDateTime.class);
         return mapping;
     }
