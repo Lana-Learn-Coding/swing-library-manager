c0a3bfd2fdbbc181c2a2c7aac5b7eb0f40c38688
Lana <lanadrahrepus2552@gmail.com>
2021-01-15 00:02:19 +0700

    refactor: remove unused annotation

--- src/main/java/io/lana/library/core/spi/datacenter/TicketDataCenter.java
+++ src/main/java/io/lana/library/core/spi/datacenter/TicketDataCenter.java
@@ -1,9 +1,7 @@
 package io.lana.library.core.spi.datacenter;
 
-import io.lana.library.core.spi.datacenter.base.RepositoryDataCenter;
 import io.lana.library.core.model.book.Ticket;
-import org.springframework.stereotype.Component;
+import io.lana.library.core.spi.datacenter.base.RepositoryDataCenter;
 
-@Component
 public interface TicketDataCenter extends RepositoryDataCenter<Integer, Ticket> {
 }
